0ded5203819447d56c16897f9938fa7653e91da2
bruno <brunocolaresdev@gmail.com>
2025-10-18 08:40:51 -0300

    java: interfaces vs herança

---
+++ java/interfaces/interfaces-vs-heranca/src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import entities.Rectangle;
+import entities.Shape;
+import entities.enums.Color;
+
+public class Program {
+	public static void main(String[] args) {
+		Shape shape = new Rectangle(Color.BLACK, 10.0, 16.0);
+		
+		
+	}
+}
